dda5d701d5475bf7fa98304ec099ba6d1a3bd87b
OmarBarba <84804799+OmarBarba@users.noreply.github.com>
2023-10-07 16:35:57 -0600

    ejemplo de ordenamiento por intercambio

---
+++ Ordenamientos_internos/03_Intercambio.py
@@ -0,0 +1,17 @@
+def bubble_sort(arr):
+    n = len(arr)
+    
+    # Realizamos n-1 pasadas a través de la lista.
+    for i in range(n - 1):
+        # En cada pasada, comparamos y, si es necesario, intercambiamos elementos.
+        for j in range(0, n - i - 1):
+            # Si el elemento actual es mayor que el siguiente elemento, los intercambiamos.
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+# Ejemplo de uso:
+lista = [64, 34, 25, 12, 22, 11, 90]
+
+print("Lista desordenada:", lista)
+bubble_sort(lista)
+print("Lista ordenada:   ", lista)
