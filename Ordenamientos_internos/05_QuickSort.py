e0b5d6f72b32b70d9f648bd37cae79e143077486
OmarBarba <84804799+OmarBarba@users.noreply.github.com>
2023-10-07 16:41:28 -0600

    Ej de ordenamiento por quicksort

---
+++ Ordenamientos_internos/05_QuickSort.py
@@ -0,0 +1,14 @@
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    else:
+        pivot = arr[0]  # Elegimos el primer elemento como pivote
+        less = [x for x in arr[1:] if x <= pivot]
+        greater = [x for x in arr[1:] if x > pivot]
+        return quick_sort(less) + [pivot] + quick_sort(greater)
+
+# Ejemplo de uso:
+lista = [64, 34, 25, 12, 22, 11, 90]
+print("Lista desordenada:", lista)
+sorted_list = quick_sort(lista)
+print("Lista ordenada:   ", sorted_list)
